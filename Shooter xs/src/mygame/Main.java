b1b74a38bcab5b06611efb7a14275e31f4223961
DylanDavids <dylan.davids@live.nl>
2014-10-09 10:14:14 +0200

    Kubus uit main nu in world

--- Shooter xs/src/mygame/Main.java
+++ Shooter xs/src/mygame/Main.java
@@ -35,8 +35,6 @@ public class Main extends SimpleApplication {
     private BulletAppState bulletAppState;
     private FilterPostProcessor fpp;
     private BloomFilter bloom;
-    private Random rand = new Random();
-    private List<Kubus> cube;
     Player player;
     public Gun gun;
     
@@ -45,37 +43,6 @@ public class Main extends SimpleApplication {
         Main app = new Main();
         app.start();
     }
-
-    private void setUpKeys() {
-        inputManager.addMapping("Reposition", new KeyTrigger(KeyInput.KEY_X));
-        inputManager.addListener(actionListener, "Reposition");
-    }
-    
-    private ActionListener actionListener = new ActionListener() {
-        public void onAction(String binding, boolean value, float tpf) {
-          if (binding.equals("Reposition")) {
-            for(int i = 0; i < cube.size(); i++){
-                Vector3f newPos = new Vector3f(randomFloat(rand.nextInt(2))*5f, cube.get(i).getDimension().y, randomFloat(rand.nextInt(2))*5f);
-                cube.get(i).setPosition(newPos);
-                cube.get(i).getKubusGeom().setLocalTranslation(newPos);
-                for(int j = 0; j < cube.size(); j++){
-                    if(j != i){
-                        while(Math.abs(cube.get(i).getPosition().x - cube.get(j).getPosition().x) < cube.get(i).getDimension().x * 2
-                        && Math.abs(cube.get(i).getPosition().x - cube.get(j).getPosition().x) < cube.get(i).getDimension().x * 2){
-                            newPos = new Vector3f(randomFloat(rand.nextInt(2))*6f, cube.get(i).getDimension().y, randomFloat(rand.nextInt(2))*6f);
-                            cube.get(i).setLocalTranslation(newPos);
-                            cube.get(i).setPosition(newPos);                      
-                        }
-                    }                    
-                    //System.out.println(cube.get(j).getPosition() + " - " + cube.get(i).getPosition());
-                }  
-            }
-//            System.out.println("X: "+ (cube.get(0).getPosition().x - cube.get(1).getPosition().x));
-//            System.out.println("Z: "+ (cube.get(0).getPosition().z - cube.get(1).getPosition().z));
-//            System.out.println("Clicked X");
-          }
-        }
-    };
     
     @Override
     public void simpleInitApp() {
@@ -87,9 +54,6 @@ public class Main extends SimpleApplication {
         
         createLight();
         initViewport();
-        cube = new ArrayList<Kubus>();
-        generateCubes();
-        setUpKeys();
         
         world = new World(assetManager, bulletAppState, 500f, 500f);
 
@@ -124,7 +88,7 @@ public class Main extends SimpleApplication {
     public void initViewport(){
         fpp = new FilterPostProcessor(assetManager);
         bloom = new BloomFilter(BloomFilter.GlowMode.Objects);      
-        bloom.setDownSamplingFactor(10.0f); 
+        bloom.setDownSamplingFactor(1.0f); 
         fpp.addFilter(bloom);
         
         viewPort.addProcessor(fpp);
@@ -135,65 +99,5 @@ public class Main extends SimpleApplication {
         sun.setColor(ColorRGBA.Yellow);
         sun.setDirection(new Vector3f(-.5f,-.5f,-.5f).normalizeLocal());
         rootNode.addLight(sun);
-    }
-  
-    float randomFloat(int negOrPos){
-        int rf = rand.nextInt(20);
-        while(rf < 1){
-            rf = rand.nextInt(20);
-        }
-        if(negOrPos == 1)
-            return rand.nextFloat() + rf;
-        else
-            return rand.nextFloat() - rf;
-    }
-     
-    private void generateCubes(){
-        for(int i = 0; i < 500; i++){
-            float sizeVal = randomFloat(1);
-            if(sizeVal < 5){
-                sizeVal += 5;
-            }
-            float posX = randomFloat(rand.nextInt(3))*20f;
-            float posZ = randomFloat(rand.nextInt(3))*20f;
-            Vector3f size = new Vector3f(sizeVal, sizeVal, sizeVal);
-            Vector3f pos = new Vector3f(posX, sizeVal, posZ);
-            if(cube.isEmpty()){
-                cube.add(new Kubus(assetManager, bulletAppState, size, pos));
-                rootNode.attachChild(cube.get(i));
-            } else {
-                cube.add(new Kubus(assetManager, bulletAppState, size, pos));
-                rootNode.attachChild(cube.get(i));
-//                int counter = 0;
-//                int j = 0;
-//                while(j < cube.size()){
-//                    if(j != i){
-//                        CollisionResults results = new CollisionResults();
-//                        cube.get(j).collideWith(cube.get(i).getWorldBound(), results);
-//                        if(results.size() != 0){
-//                            while(results.size() != 0){
-//                                Vector3f newPos = new Vector3f(randomFloat(rand.nextInt(2))*6f, sizeVal, randomFloat(rand.nextInt(2))*6f);
-//                                cube.get(i).setLocalTranslation(newPos);
-//                                cube.get(i).setPosition(newPos); 
-//                                results = new CollisionResults();
-//                                cube.get(j).collideWith(cube.get(i).getWorldBound(), results);                     
-//                            }
-//                            j = 0;
-//                        } else {
-//                            j++;
-//                        }
-////                        while(Math.abs(cube.get(j).getPosition().x - cube.get(i).getPosition().x) < cube.get(i).getDimension().x * 2
-////                        && Math.abs(cube.get(j).getPosition().z - cube.get(i).getPosition().z) < cube.get(i).getDimension().z * 2){
-////                            Vector3f newPos = new Vector3f(randomFloat(rand.nextInt(2))*6f, sizeVal, randomFloat(rand.nextInt(2))*6f);
-////                            cube.get(i).setLocalTranslation(newPos);
-////                            cube.get(i).setPosition(newPos);                      
-////                        }
-//                    } else {
-//                        j++;
-//                    }                    
-////                    System.out.println(cube.get(j).getPosition() + " - " + cube.get(i).getPosition());
-//                }
-            }       
-        }  
     }
 }
--- Shooter xs/src/mygame/World.java
+++ Shooter xs/src/mygame/World.java
@@ -11,10 +11,14 @@ import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState.FaceCullMode;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -22,6 +26,7 @@ import com.jme3.scene.shape.Box;
  */
 public class World extends Node {
     private AssetManager am;
+    private BulletAppState BAS;
     private Box floor;
     private Geometry floorGeom;
     private Material floorMat;
@@ -29,9 +34,12 @@ public class World extends Node {
     private float floorL;
     private Box wall;
     private Geometry wallGeom;
+    private Random rand = new Random();
+    private List<Kubus> cube;
     
     World(AssetManager assetManager, BulletAppState bulletAppState, float floorW, float floorL){
         am = assetManager;
+        BAS = bulletAppState;
        
         floor = new Box(floorW, 0.01f, floorL);
         floorGeom = new Geometry("Box", floor);
@@ -40,6 +48,8 @@ public class World extends Node {
         floorGeom.addControl(RBC);
         bulletAppState.getPhysicsSpace().add(RBC);
         
+        cube = new ArrayList<Kubus>();
+        generateCubes(500);
         initMaterial(am);
         
         attachChild(floorGeom);
@@ -54,5 +64,36 @@ public class World extends Node {
         floorMat.getAdditionalRenderState().setFaceCullMode(FaceCullMode.Off);
         
         floorGeom.setMaterial(floorMat);
+    }
+    
+    float randomFloat(int negOrPos){
+        int rf = rand.nextInt(20);
+        while(rf < 1){
+            rf = rand.nextInt(20);
+        }
+        if(negOrPos == 1)
+            return rand.nextFloat() + rf;
+        else
+            return rand.nextFloat() - rf;
+    }
+     
+    private void generateCubes(int amount){
+        for(int i = 0; i < amount; i++){
+            float sizeVal = randomFloat(1);
+            if(sizeVal < 5){
+                sizeVal += 5;
+            }
+            float posX = randomFloat(rand.nextInt(3))*20f;
+            float posZ = randomFloat(rand.nextInt(3))*20f;
+            Vector3f size = new Vector3f(sizeVal, sizeVal, sizeVal);
+            Vector3f pos = new Vector3f(posX, sizeVal, posZ);
+            if(cube.isEmpty()){
+                cube.add(new Kubus(am, BAS, size, pos));
+                attachChild(cube.get(i));
+            } else {
+                cube.add(new Kubus(am, BAS, size, pos));
+                attachChild(cube.get(i));
+            }       
+        }  
     }
 }
