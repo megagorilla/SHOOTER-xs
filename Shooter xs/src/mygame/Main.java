58f1b79d20394352e11bca9c2d02eeddcb2c5ed1
DylanDavids <dylan.davids@live.nl>
2014-11-08 17:16:39 +0100

    Startscherm werkend met game.

--- Shooter xs/src/mygame/Main.java
+++ Shooter xs/src/mygame/Main.java
@@ -1,247 +1,71 @@
-/**
- * Door: Dylan Davids & Sander de jong
- * Datum(Start) 25 september 2014
- * Project Shooter xs
- * mini shooter game als eigen idee graphics
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
 package mygame;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.audio.AudioNode;
-import com.jme3.bullet.BulletAppState;
-import com.jme3.bullet.PhysicsSpace;
-import com.jme3.collision.CollisionResult;
-import com.jme3.bullet.collision.PhysicsCollisionEvent;
-import com.jme3.bullet.collision.PhysicsCollisionListener;
-import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.collision.Collidable;
-import com.jme3.collision.CollisionResults;
-import com.jme3.font.BitmapText;
-import com.jme3.light.DirectionalLight;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.FastMath;
-import com.jme3.math.Quaternion;
-import com.jme3.math.Vector3f;
-import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.shadow.DirectionalLightShadowRenderer;
-import com.jme3.shadow.EdgeFilteringMode;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.Trigger;
+import com.jme3.system.AppSettings;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
 
 /**
- * test
- * @author normenhansen
+ *
+ * @author Dylankuyjt67hr
  */
-public class Main extends SimpleApplication {
-    private World world;
-    private BulletAppState bulletAppState;
-    Player player;
-    public Gun gun;
-    private AudioNode ammoPickup;
-    AudioNode enemyDying;
-    private List<AmmoCrate> ammoCrates = new ArrayList<AmmoCrate>();
-    BitmapText currentMagSize;
-    BitmapText currentScore;
-    private int score = 0;
-    private List<Enemy> Enemies = new ArrayList<Enemy>();
-    
-    public static void main(String[] args) {
+public class Main extends SimpleApplication implements ScreenController {
 
-        Main app = new Main();
-        app.start();
-    }
+    private Trigger start_trigger = new KeyTrigger(KeyInput.KEY_SPACE);
+    private boolean isRunning = false; // starts at startscreen
+    private GameRunningState gameRunningState;
+    private StartScreenState startScreenState;
     
     @Override
     public void simpleInitApp() {
-        //StartScreenState startScreenState = new StartScreenState(this);
-        //stateManager.attach(startScreenState);
-        
-        ammoPickup = new AudioNode(assetManager, "Sounds/reload.wav");
-        enemyDying = new AudioNode(assetManager, "Sounds/deathEnemy.wav");
-        viewPort.setBackgroundColor(ColorRGBA.Cyan);
-        bulletAppState = new BulletAppState();
-        bulletAppState.setThreadingType(BulletAppState.ThreadingType.PARALLEL);
-        stateManager.attach(bulletAppState);
+        setDisplayFps(false);
+        setDisplayStatView(false);
 
-        world = new World(assetManager, bulletAppState, 500f, 500f);
-        rootNode.attachChild(world);
-        
-        flyCam.setMoveSpeed(50);
-        gun = new Gun(assetManager, viewPort, bulletAppState, cam);
-        player = new Player(bulletAppState, inputManager,assetManager, cam, gun);
-        rootNode.attachChild(player);
-        rootNode.attachChild(gun);
-        createLight();
-        
-        if(false){ //enable/disable debug mode
-            bulletAppState.getPhysicsSpace().enableDebug(assetManager);
-            player.debug();
-            player.setMinigun();
+        gameRunningState    = new GameRunningState(this);
+        startScreenState    = new StartScreenState(this);
+        startScreenState.simpleInitApp();      
+        stateManager.attach(startScreenState);
+
+        inputManager.addMapping("Start", start_trigger);
+        inputManager.addListener(actionListener, new String[]{"Start"});
+    }
+    
+    private ActionListener actionListener = new ActionListener() {
+      public void onAction(String name, boolean isPressed, float tpf) {
+        if (name.equals("Start") && !isPressed) {
+          if (!isRunning) {
+            stateManager.attach(gameRunningState);
+            stateManager.detach(startScreenState);
+            isRunning = true;
+          }
         }
-        
-        for(int i = 0; i < 5; i++){
-            Enemies.add(new Enemy("Enemy "+i, assetManager, bulletAppState, randomVectorBetween(-250, 250, 2)));            
-            rootNode.attachChild(Enemies.get(Enemies.size()-1));            
-        }
-        
-        CollisionResults results = new CollisionResults();
-        for(Enemy x : Enemies){            
-            world.getWorldNode().collideWith(x.getWorldBound(), results);
-            Vector3f currentPos = x.getPosition();
-            if(results.size() > 1){
-                x.setLocalTranslation(currentPos.x + 1f, currentPos.y, currentPos.z + 1f);
-            }
-        }
-        
-        /** Write text on the screen (HUD) */
-        guiNode.detachAllChildren();
-        guiFont = assetManager.loadFont("Interface/Fonts/Aharoni.fnt");
-        
-        currentMagSize = new BitmapText(guiFont, false);
-        currentMagSize.setSize(guiFont.getCharSet().getRenderedSize());
-        currentMagSize.setText("Ammo: " +player.getInMagazine() + " / " + player.getMagsize());
-        currentMagSize.setColor(ColorRGBA.White);
-        currentMagSize.setSize(50);
-        currentMagSize.setLocalTranslation(0, this.settings.getHeight() -currentMagSize.getLineHeight(), 0);
-        
-        currentScore = new BitmapText(guiFont, false);
-        currentScore.setSize(guiFont.getCharSet().getRenderedSize());
-        currentScore.setText("Score: " +score);
-        currentScore.setSize(50);
-        currentScore.setLocalTranslation(0, this.settings.getHeight(), 0);
-        
-        guiNode.attachChild(currentMagSize);
-        guiNode.attachChild(currentScore);
+      }
+    };    
+    
+    public static void main(String[] args) {
+       Main app = new Main();
+       app.start();
+    }
+    
+    public void bind(Nifty nifty, Screen screen) {
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public void simpleUpdate(float tpf) {
-        currentMagSize.setText("Ammo: " +player.getInMagazine() + " / " + player.getMagsize());
-        currentScore.setText("Score: " +score);
-        player.update(tpf);
-        ammoDrop();
-        if(player.getInMagazine() < player.getMagsize())
-            ammoCratePickup();
-        
-        enemyMovement();
-        collisionBetweenBulletAndEnemy();
-    }
-    
-    private void enemyMovement(){
-        for(Enemy x : Enemies){
-            Vector3f playerLoc = player.getCamLocation();                
-            Vector3f curLoc = x.CC.getPhysicsLocation();
-            float distance = curLoc.distance(playerLoc);          
-
-            float moveX = FastMath.floor(playerLoc.x) - FastMath.floor(curLoc.x);
-            float moveZ = FastMath.floor(playerLoc.z) - FastMath.floor(curLoc.z);
-            float moveTotal = FastMath.abs(moveX) + FastMath.abs(moveZ);
-
-            moveX = (moveX / moveTotal) * 0.5f;
-            moveZ = (moveZ / moveTotal) * 0.5f;
-
-            //while (distance > 10) {                
-                x.CC.setPhysicsLocation(new Vector3f((curLoc.x + moveX), curLoc.y, (curLoc.z + moveZ)));//.addLocal(moveX, moveY);
-                //curLoc = x.enemyGeom.getLocalTranslation();
-                //distance = curLoc.distance(playerLoc);
-            //}
-            
-            playerLoc.setY(x.getLocalTranslation().y);
-            x.lookAt(playerLoc, new Vector3f(0, 1, 0));
-            x.playSound();
-        }
-    }
-    
-    private void collisionBetweenBulletAndEnemy(){
-        CollisionResults results = new CollisionResults();
-        
-        outerloop: for(Geometry x: player.getGun().getBullets()){
-            if(player.getGun().getBullets().indexOf(x) > 0){
-                for(Enemy e : Enemies){
-                    e.enemyGeom.collideWith(x.getWorldBound(), results);
-                    for (int i = 0; i < results.size(); i++) {
-                        // Remove bullet
-                        player.getGun().deleteBullet(player.getGun().getBullets().size() - 1);
-                        e.setHealth(e.getHealth() - 20);
-                        if(e.getHealth() <= 0){
-                            e.finishOfEnemy(e);
-                            Enemies.remove(Enemies.indexOf(e));
-                            newEnemy();
-                            newEnemy();
-                            score += 10;
-                            enemyDying.playInstance();
-                        }
-                        break outerloop;
-                     }
-                }
-            }
-        }
-    }
-    
-    private void newEnemy(){
-        Enemies.add(new Enemy("Enemy "+ Enemies.size(), assetManager, bulletAppState, randomVectorBetween(-250, 250, 2)));            
-        rootNode.attachChild(Enemies.get(Enemies.size()-1));
-    }
-    
-    private void ammoDrop(){
-        if(ammoCrates.size()< 100){
-            ammoCrates.add(new AmmoCrate(bulletAppState, assetManager,randomVectorBetween(-250, 250, 100)));
-            rootNode.attachChild(ammoCrates.get(ammoCrates.size()-1));
-        }
-    }
-    
-    private Vector3f randomVectorBetween(int min, int max, float y){
-        Vector3f locVector = new Vector3f();
-        locVector.y = y;
-        
-        Random rand = new Random();
-        float finalX = rand.nextFloat() * (max - min) + min;
-        locVector.x = finalX;
-        
-        rand = new Random();
-        finalX = rand.nextFloat() * (max - min) + min;
-        locVector.z = finalX;
-        
-        return locVector;
-    }
-    
-    private void ammoCratePickup(){
-        Vector3f playerLocation = player.getCamLocation();
-        for(int i = 0; i< ammoCrates.size();i++){
-            Vector3f crateLocation = ammoCrates.get(i).getLocalTranslation();
-            if(playerLocation.distance(crateLocation) < 5f){
-                player.addammo(10);
-                rootNode.detachChild(ammoCrates.get(i));
-                ammoCrates.get(i).destroyControl();
-                ammoCrates.remove(i);
-                ammoPickup.playInstance();
-            }
-        }
-    }
-    
-    @Override
-    public void simpleRender(RenderManager rm) {
-    }
-    
-    public PhysicsSpace getPhysicsSpace() {
-        return bulletAppState.getPhysicsSpace();
+    public void onStartScreen() {
+      //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void createLight(){
-        DirectionalLight sun = new DirectionalLight();
-        sun.setColor(ColorRGBA.Yellow);
-        sun.setDirection(new Vector3f(-.5f,-.5f,-.5f).normalizeLocal());
-        rootNode.addLight(sun);
-        
-        /* this shadow needs a directional light */
-        DirectionalLightShadowRenderer dlsr = new DirectionalLightShadowRenderer(assetManager, 1024, 4);
-        dlsr.setLight(sun);
-        dlsr.setEdgeFilteringMode(EdgeFilteringMode.Bilinear);
-        dlsr.setShadowIntensity(0.5f);
-        viewPort.addProcessor(dlsr); 
-        
-   }
-
+    public void onEndScreen() {
+      //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
--- Shooter xs/src/mygame/StartScreenState.java
+++ Shooter xs/src/mygame/StartScreenState.java
@@ -4,58 +4,79 @@
  */
 package mygame;
 
-import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppState;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.asset.AssetManager;
-import com.jme3.math.ColorRGBA;
+import com.jme3.audio.AudioRenderer;
+import com.jme3.input.FlyByCamera;
+import com.jme3.input.InputManager;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Node;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
 
 /**
  *
  * @author Dylankuyjt67hr
  */
-public class StartScreenState extends AbstractAppState {
+public class StartScreenState extends AbstractAppState implements ScreenController {
  
-  private ViewPort viewPort;
-  private Node rootNode;
-  private Node guiNode;
-  private AssetManager assetManager;
-  private Node localRootNode = new Node("Start Screen RootNode");
-  private Node localGuiNode = new Node("Start Screen GuiNode");
-  private final ColorRGBA backgroundColor = ColorRGBA.Gray;  
- 
-public StartScreenState(SimpleApplication app){
-    this.rootNode     = app.getRootNode();
-    this.viewPort     = app.getViewPort();
-    this.guiNode      = app.getGuiNode();
-    this.assetManager = app.getAssetManager();  
-  }
- 
-  @Override
-  public void initialize(AppStateManager stateManager, Application app) {
-    super.initialize(stateManager, app);
- 
-    rootNode.attachChild(localRootNode);
-    guiNode.attachChild(localGuiNode);
-    viewPort.setBackgroundColor(backgroundColor);
- 
-    //nifty.fromXml("Interface/myGui.xml", "start", startScreenState);
-  }
- 
-  @Override
-  public void update(float tpf) {
-    /** any main loop action happens here */
-  }
- 
-  @Override
-  public void cleanup() {
-    rootNode.detachChild(localRootNode);
-    guiNode.detachChild(localGuiNode);
- 
-    super.cleanup();
-  }
+    private ViewPort viewPort;
+    private Node rootNode;
+    private Node guiNode;
+    private AssetManager assetManager;  
+    private InputManager inputManager;  
+    private AudioRenderer audioRenderer;
+    private FlyByCamera flyCam;
+    private Nifty nifty;
+
+    public StartScreenState(SimpleApplication app) {
+        this.rootNode     = app.getRootNode();
+        this.viewPort     = app.getViewPort();
+        this.guiNode      = app.getGuiNode();
+        this.assetManager = app.getAssetManager();
+        this.inputManager = app.getInputManager();
+        this.audioRenderer= app.getAudioRenderer();
+        this.flyCam       = app.getFlyByCamera();
+    }
+
+    public void simpleInitApp() {                
+        NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager, inputManager, audioRenderer, viewPort);
+        
+        nifty = niftyDisplay.getNifty();
+        nifty.fromXml("Interface/Nifty/StartScreen.xml", "start", this);
+
+        // attach the nifty display to the gui view port as a processor
+        viewPort.addProcessor(niftyDisplay);
+
+        // disable the fly cam
+        flyCam.setEnabled(false);
+        flyCam.setDragToRotate(true);
+        
+        inputManager.setCursorVisible(true);        
+    }
+
+    public void bind(Nifty nifty, Screen screen) {
+    }
+
+    public void onStartScreen() {
+    }
+
+    public void onEndScreen() {
+    }
+    
+   @Override
+   public void stateAttached(AppStateManager stateManager) {       
+       
+   }
+
+   @Override
+   public void stateDetached(AppStateManager stateManager) {
+      nifty.exit();
+   }   
  
 }
